7112efbf04850d07664539af2ed46ad082b905f3
farhadhsn <farhadhsn8@gmail.com>
2021-04-17 21:03:53 +0430

    =create prj - create migrations and models - write some functions for relation between tables

---
+++ LF_prj/backend/src/database/migrations/2021_04_17_155923_create_threads_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->string('name');
+            $table->boolean('flag')->default(1);
+            $table->foreignId('user_id')
+                    ->constraint()->onDelete('cascade');
+            $table->foreignId('channel_id')
+                    ->constraint()->onDelete('cascade');
+            //best answer id
+            $table->foreignId('answer_id')
+                    ->constraint()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
